bcc0607a729bf4cab4c92dc459eb1e7098dedb25
Javier Sanchez Mendoza <javier.sanchezm@sat.gob.mx>
2022-09-14 15:34:44 -0500

    init update contador

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,19 @@
 <script>
   export let name;
   let year = 2022;
+
+  function incrementYear(){
+    year += 1;
+  }
 </script>
 
 <main>
   <h1>Hello {name}, {year}</h1>
+  <button on:click="{incrementYear}">Change Yeard</button>
 </main>
 
 <style>
   h1{
-    color: purple;
+    color: orange;
   }
 </style>
