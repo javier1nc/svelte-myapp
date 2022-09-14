64d5ff191faadab2cc0975273f0fff298a38464a
Javier Sanchez Mendoza <javier.sanchezm@sat.gob.mx>
2022-09-14 16:41:45 -0500

    init app updated

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,21 @@
   export let name;
   let year = 2022;
 
+  $: uppecaseName = name.toUpperCase();
+  $: name;
+
   function incrementYear(){
     year += 1;
+  }
+  function  changeName() {
+    name = 'Javier';
   }
 </script>
 
 <main>
-  <h1>Hello {name}, {year}</h1>
+  <h1>Hello {uppecaseName}, {year}</h1>
   <button on:click="{incrementYear}">Change Yeard</button>
+  <button on:click="{changeName}">Change Name</button>
 </main>
 
 <style>
