1b66f314682f6ae5959537a91acecba5b3294cec
Javier Sanchez Mendoza <javier.sanchezm@sat.gob.mx>
2022-09-14 10:54:22 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  export let name;
+</script>
+
+<main>
+  <h1>Hello {name}</h1>
+</main>
+
+<style>
+  h1{
+    color: purple;
+  }
+</style>
